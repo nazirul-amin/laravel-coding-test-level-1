a776a4bdedbab2b03ccb2aac9bd6d7faefd51a30
Muhammad Nazirul Amin bin Khairul Anwar <nazirul@time.com.my>
2022-09-07 18:56:53 +0800

    Add mutator for created_at and updated_at field

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Carbon\Carbon;
 use Database\Factories\EventFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -23,6 +24,20 @@ class Event extends Model
 
     protected static function newEvent(){
         return EventFactory::new();
+    }
+
+    protected function createdAt(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => Carbon::parse($value)->format('d/m/Y'),
+        );
+    }
+
+    protected function updatedAt(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => Carbon::parse($value)->format('d/m/Y'),
+        );
     }
 
     public function scopeActive($query)
